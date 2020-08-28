bf6e1b8dfcea783c6b9a46541c6534cafd0da741
hansanguk0222 <hansanguk0222@gmail.com>
2020-08-28 11:42:08 +0900

    비동기를 이용한 입출력으로 수정

--- CPU/CPU_javascript/index.js
+++ CPU/CPU_javascript/index.js
@@ -1,3 +1,12 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const input = []
+let which = 0
+
 const CPU = require('./CPU')
 const Memory = require('./Memory')
 
@@ -5,98 +14,117 @@ const memory = new Memory()
 const cpu = new CPU()
 
 const parseValue = (str) => {
-    return str.split(', ')
+    return str.split(' ')
 }
 
-const make_cpu_work = (memory) => {
+const funcP = (x) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(_ => {
+            console.log(x)
+            resolve(x)
+        })
+    }, 1000)
+}
+
+const make_cpu_work = async (memory) => {
     for(let i = 0; i < memory.length; i++) {
-        (function(j) {
-            setTimeout(() => {
-                cpu.fetch(memory)
-                const arr = cpu.IR.split(' ')
-                const instruction = arr[0]
-                
-                if(arr.length === 2) {
-                    switch(instruction) {
-                        case 'LOAD': 
-                            cpu.LOAD(memory) 
-                            break
-                        case 'ADD1':
-                            cpu.ADD1(memory)
-                            break
-                        case 'SUB1':
-                            cpu.SUB1(memory)
-                            break
-                        case 'MUL1':
-                            cpu.MUL1(memory)
-                            break
-                        case 'DIV1':
-                            cpu.DIV1(memory)
-                            break
-                        case 'STOR':
-                            cpu.STOR(memory)
-                            break
-                    }
-                }
-                else if(arr.length === 3) {
-                    switch(instruction) {
-                        case 'MOV':
-                            cpu.MOV(memory)
-                            break
-                        case 'ADD2':
-                            cpu.ADD2(memory)
-                            break
-                        case 'SUB2':
-                            cpu.SUB2(memory)
-                            break
-                        case 'MUL2':
-                            cpu.MUL2(memory)
-                            break
-                        case 'DIV2':
-                            cpu.DIV2(memory)
-                            break
-                    }
-                }
-                else if(arr.length === 4) {
-                    switch(instruction) {
-                        case 'ADD3':
-                            cpu.ADD3(memory)
-                            break
-                        case 'SUB3':
-                            cpu.SUB3(memory)
-                            break
-                        case 'MUL3':
-                            cpu.MUL3(memory)
-                            break
-                        case 'DIV3':
-                            cpu.DIV3(memory)
-                            break
-                    }
-                }
-            }, 2000 * (j + 1))
-        })(i);
-        (function(j) {
-            setTimeout(() =>{
-                console.clear()
-            }, 2000 * (j + 2))
-        })(i)
+        cpu.fetch(memory)
+
+        await cpu.show(memory)
+        await cpu.log_clear()
+
+        const arr = cpu.IR.split(' ')
+        const instruction = arr[0]
+
+        switch(instruction) {
+            case 'LOAD':
+                cpu.LOAD(memory)
+                break
+            case 'ADD1':
+                cpu.ADD1(memory)
+                break
+            case 'SUB1':
+                cpu.SUB1(memory)
+                break
+            case 'MUL1':
+                cpu.MUL1(memory)
+                break
+            case 'DIV1':
+                cpu.DIV1(memory)
+                break
+            case 'STOR':
+                cpu.STOR(memory)
+                break
+            case 'MOV':
+                cpu.MOV(memory)
+                break
+            case 'ADD2':
+                cpu.ADD2(memory)
+                break
+            case 'SUB2':
+                cpu.SUB2(memory)
+                break
+            case 'MUL2':
+                cpu.MUL2(memory)
+                break
+            case 'DIV2':
+                cpu.DIV2(memory)
+                break
+            case 'ADD3':
+                cpu.ADD3(memory)
+                break
+            case 'SUB3':
+                cpu.SUB3(memory)
+                break
+            case 'MUL3':
+                cpu.MUL3(memory)
+                break
+            case 'DIV3':
+                cpu.DIV3(memory)
+                break
+        }
+
+        await cpu.show(memory)
+        await cpu.log_clear()
     }
+    
+    process.exit()
 }
 
-const pipe = (...fns) => (value) => fns.reduce((acc, fn) => fn(acc), value)
-
-const instruction1 = 'LOAD A, ADD1 B, STOR T, LOAD C, SUB1 D, MUL1 T, STOR X'
-const instruction2 = 'MOV R1 A, ADD2 R1 B, MOV R2 C, SUB2 R2 D, MUL2 R1 R2, MOV X R1'
-const instruction3 = 'ADD3 R1 A B, SUB3 R2 C D, MUL3 X R1 R2'
-memory.mem['A'] = 1
-memory.mem['B'] = 2
-memory.mem['T'] = 0
-memory.mem['C'] = 6
-memory.mem['D'] = 3
-memory.mem['X'] = 0
-
-const res = pipe(
-    x => parseValue(x), //명령어 파싱
-    x => memory.load_instruction(x), //메모리에 명령어 올리기
-    x => make_cpu_work(x)
-)(instruction3)
+rl.setPrompt('1주소, 2주소, 3주소 중에서 입력해주세요 >> ')
+rl.prompt()
+rl.on('line', line =>{
+    if(which === 0) {
+        if(1 <= parseInt(line) && parseInt(line) <= 3) {
+            which = parseInt(line)
+            rl.setPrompt(`${which}주소로 입력 하였습니다. quit 입력 시 명령어 입력 받기 완료 후 메모리 자동 임의 값으로 초기화 >> `)
+        }
+    }
+    else {
+        const arr = parseValue(line)
+        if(which === 1 && (arr[0] === 'LOAD' || arr[0] === 'ADD1' || arr[0] === 'SUB1' || arr[0] === 'MUL1' || arr[0] === 'DIV1' || arr[0] === 'STOR')) {
+            arr.shift()
+            memory.set_Memory(arr)
+            input.push(line)
+        }
+        else if(which === 2 && (arr[0] === 'MOV' || arr[0] === 'ADD2' || arr[0] === 'SUB2' || arr[0] === 'MUL2' || arr[0] === 'DIV2')) {
+            memory.set_Memory(arr)
+            input.push(line)
+        }
+        else if(which === 3 && (arr[0] === 'ADD3' || arr[0] === 'SUB3' || arr[0] === 'MUL3' || arr[0] === 'DIV3')) {
+            memory.set_Memory(arr)
+            input.push(line)
+        }
+        else if(line === 'quit') {
+            rl.setPrompt('')
+            memory.load_instruction(input)
+            rl.close()
+        }
+        else {
+            console.log(`${which}주소인데 잘못된 명령어를 입력하셨습니다.`)
+        }
+    }
+    rl.prompt()
+}).on('close', () => {
+    make_cpu_work(memory)
+})
