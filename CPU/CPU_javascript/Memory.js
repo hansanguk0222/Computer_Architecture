15cc2e6cbe3c964f9212e9f52b32bc5100aa2147
hansanguk0222 <hansanguk0222@gmail.com>
2020-08-28 11:41:53 +0900

    메모리를 초기화 랜덤으로 하게 수정

--- CPU/CPU_javascript/Memory.js
+++ CPU/CPU_javascript/Memory.js
@@ -11,6 +11,14 @@ class Memory {
         })
         return this
     }
+
+    set_Memory(arr) {
+        arr.map(x => {
+            if(x[0] !== 'R') {
+                this.mem[x] = Math.floor(Math.random() * 10) + 1
+            }
+        })
+    }
 }
 
 module.exports = Memory
